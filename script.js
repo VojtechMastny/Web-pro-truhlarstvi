0aaa927bf53acbf26b42cbe70c38256600ac1606
VojtechMastny <86232787+VojtechMastny@users.noreply.github.com>
2024-01-03 06:34:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-const buttons = document.querySelectorAll("[data-carousel-button]")
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1
-        const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-        const activeSlide = slides.querySelector("[data-active]")
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset
-        if (newIndex < 0) newIndex = slides.children.length - 1
-        if (newIndex >= slides.children.length) newIndex = 0
-
-        slides.children[newIndex].dataset.active = true
-        delete activeSlide.dataset.active
-    })
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 })
 
-setInterval(changePhoto, 4000);
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
+
 
 function changePhoto()
 {
@@ -27,4 +24,10 @@ function changePhoto()
 
     slides.children[newIndex].dataset.active = true
      delete activeSlide.dataset.active
+     
 }
+
+setInterval(changePhoto, 4000);
+
+
+
