bf3c0d7b27339401d412600781e62adcabc2be3a
VojtechMastny <86232787+VojtechMastny@users.noreply.github.com>
2024-02-23 06:42:38 +0100

    Add files via upload

---
+++ Web-pro-truhlarstvi-main_2/Web-pro-truhlarstvi-main/truhlarstvi-sticky-main/login.php
@@ -0,0 +1,91 @@
+<html lang="cs">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/styles/login.css">
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+</head>
+
+<body>
+    <?php
+session_start();
+  $buttonText = 'Přihlášení';
+$loginText = isset($_SESSION['admin']) ? 'Účet' : 'Přihlášení';
+?>
+    <header>
+        <div class="navber">
+            <nav class="nav">
+                <ul class="nav-menu">
+                    <li class="nav-item">
+                        <a href="index.html" class="nav-link">Domů</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Galerie</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Kalkulace</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="kontakty.html" class="nav-link">Kontakty</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="login.html" class="nav-link"><?php echo $buttonText; ?></a>
+                    </li>
+
+                </ul>
+
+                <div class="hamburger">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+
+    <div class="login">
+        <h1>Přihlášení</h1>
+        <form action="authenticate.php" method="post">
+            <label for="username">
+                <i class="fas fa-user"></i>
+            </label>
+            <input type="text" name="username" placeholder="Jméno" id="username" required>
+            <label for="password">
+                <i class="fas fa-lock"></i>
+            </label>
+            <input type="password" name="password" placeholder="Heslo" id="password" required>
+            <input type="submit" value="Přihlásit se">
+        </form>
+        <a href="register.html" class="log-link">Zaregistrovat se</a>
+    </div>
+
+
+
+
+
+    <footer class="footer-2">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col">
+                    <ul>
+                        <li><a href="index.html">Domů</a></li>
+                        <li><a href="#">Galerie</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="kontakty.html">Kontakty</a></li>
+
+                    </ul>
+                    <h4>Stanislav malec</h4>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="assets/scripts/script.js"></script>
+</body>
+
+</html>
